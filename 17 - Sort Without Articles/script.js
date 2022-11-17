b2ed755b4df79ef2cf85551de38dbb6fb51c9c51
Hamna R <hamnaraeel@gmail.com>
2022-11-17 22:26:59 +0500

    Added 17 challenge: Sorting Bands

---
+++ 17 - Sort Without Articles/script.js
@@ -0,0 +1,15 @@
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+function strip(bandName) {
+  return bandName.replace(/^(a |the |an)/, '').trim();
+}
+
+const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+
+document.querySelector('#bands').innerHTML = 
+  sortedBands
+    .map(bands => `<li>${bands}</li>`)
+    .join('');
+
+
+console.log(sortedBands);
